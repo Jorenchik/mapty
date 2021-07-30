3c3222a102a76d48bbe59511e053b67579dbb4ab
jorenchik <jorens.stekels@gmail.com>
2021-07-30 16:57:31 +0300

    Sidebar and map components

---
+++ src/components/Map/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Styles
+import { Content, Wrapper } from "./Map.styles";
+
+const Map = () => (
+  <Wrapper>
+    <Content></Content>
+  </Wrapper>
+);
+
+export default Map;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Styles
+import { Content, Wrapper } from "../Map/Map.styles";
+
+const SideBar = () => (
+  <Wrapper>
+    <Content></Content>
+  </Wrapper>
+);
+
+export default SideBar;
